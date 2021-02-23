b7bfc1e49b49ceacf1431f39b2fa143fdb3dddb2
Troy Gaylord <troy.gaylord@gmail.com>
2021-02-23 09:56:33 -0500

    Cleanup

--- esp32-bootloader-and-factory/src/main.cpp
+++ esp32-bootloader-and-factory/src/main.cpp
@@ -33,8 +33,6 @@ void   onGet_root();
 void   on_fileUpload();
 void   onGet_listFiles();
 void   on_file();
-// void   onPost_firmwareUpdate();
-// void   onPost_firmwareUpdater();
 void   onGet_reboot();
 void   onGet_logout();
 void   onGet_loggedOut();
@@ -71,7 +69,7 @@ void initSpiffs() {
 
 String pageProcessor(const String& var) {
   Serial.println("pageProcessor(...)");
-  // LEFT OFF HERE> Need something like this.
+
   if (var == "FIRMWARE") {
     return FACTORY_FIRMWARE_VERSION;
   }
@@ -116,17 +114,13 @@ void initRoutes() {
   Serial.println("initRoutes()");
 
   _server.on("/",                HTTP_GET,  onGet_root);
-  // _server.onFileUpload(                     on_fileUpload);
 
-  // _server.on("/upload-file", HTTP_POST, []() {
   _server.on("/", HTTP_POST, []() {
     Serial.println("HTTP_POST: /");
     _server.send(200, "text/plain", "");
   }, handleFileUpload);
 
   _server.on("/list-files",      HTTP_GET,  onGet_listFiles);
-  // _server.on("/update-firmware", HTTP_POST, onPost_firmwareUpdate, onPost_firmwareUpdater);
-
     _server.on("/update-firmware", HTTP_POST, []() {
       Serial.println("/update-firmware (1)");
       _server.sendHeader("Connection", "close");
@@ -179,7 +173,6 @@ void handleFileUpload() {
     filename = String();
   } else if (upload.status == UPLOAD_FILE_WRITE) {
     Serial.println("UPLOAD_FILE_WRITE");
-    //Serial.print("handleFileUpload Data: "); Serial.println(upload.currentSize);
     if (fsUploadFile) {
       fsUploadFile.write(upload.buf, upload.currentSize);
     }
@@ -195,43 +188,8 @@ void handleFileUpload() {
 
 void onGet_root() {
   Serial.println("onGet_root()");
-  // LEFT OFF HERE> process the page before sending it out.
-
-  // _server.send_P(200, "text/html", _admin_html, pageProcessor);
   _server.send_P(200, "text/html", _admin_html);
 }
-
-// // void on_fileUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
-// void on_fileUpload() {
-//   // LEFT OFF HERE> CONVERT to WebServer
-
-//   Serial.println("task_net::onFileUpload(...)");
-//   // String logmessage; // = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
-//   // Serial.println(logmessage);
-
-//   if (!index) {
-//     // logmessage = "Upload Start: " + String(filename);
-//     // open the file on first call and store the file handle in the request object
-//     _tempFile = SPIFFS.open("/" + filename, "w");
-//     Serial.printf("Upload Start: %s\n", filename.c_str());
-//   }
-
-//   if (len) {
-//     // stream the incoming chunk to the opened file
-//     _tempFile.write(data, len);
-//     // logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
-//     // Serial.println(logmessage);
-//   }
-
-//   if (final) {
-//     // logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
-//     // close the file handle as the upload is now done
-//     _tempFile.close();
-//     // Serial.println(logmessage);
-//     Serial.println(F("Upload Complete."));
-//     // request->redirect("/");
-//   }
-// }
 
 void onGet_listFiles() {
   Serial.println("onGet_listFiles()");
@@ -269,58 +227,6 @@ String listFiles(bool ishtml) {
   foundfile.close();
   return returnText;
 }
-
-// void onPost_firmwareUpdate() {
-//    Serial.println("onPost_firmwareUpdate(...)");
-//   // The request handler is triggered after the upload has finished... 
-//   // Create the response, add header, and send response
-//   // AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", Update.hasError() ? "FAIL" : "OK");
-//   AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html",
-//     Update.hasError() ? _ota_fail_html : _ota_success_html);
-
-//   response->addHeader("Connection", "close");
-//   response->addHeader("Access-Control-Allow-Origin", "*");
-//   request->send(response);
-// }
-
-// // void onPost_firmwareUpdater(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
-// void onPost_firmwareUpdater() {
-//   // LEFT OFF HERE> Convert
-
-//   // Upload handler chunks in data.
-//   if (!index) {
-//     Serial.printf("UploadStart: %s\n", filename.c_str());
-//     // calculate sketch space required for the update
-//     uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
-//     // _otaState = NET_JOB_STATUS::START;
-//     // _onNetStatusUpdateCb(NET_JOB::OTA, _otaState);
-
-//     // Start with max available size
-//     if (!Update.begin(maxSketchSpace)) {
-//       Update.printError(Serial);
-//     }
-//   }
-  
-//   // Write chunked data to the free sketch space
-//   if (Update.write(data, len) != len) {
-//     Update.printError(Serial);
-//   }
-
-//   // If the final flag is set then this is the last frame of data
-//   if (final) {
-//     // True to set the size to the current progress
-//     if (Update.end(true)) {
-//       // _otaState = NET_JOB_STATUS::COMPLETE;
-//       // _onNetStatusUpdateCb(NET_JOB::OTA, _otaState);
-//       // _onNetStatusUpdateCb(NET_JOB::REBOOT, NET_JOB_STATUS::UNKNOWN);
-
-//     } else {
-//       // _otaState = NET_JOB_STATUS::ERROR;
-//       Update.printError(Serial);
-//       // _onNetStatusUpdateCb(NET_JOB::OTA, _otaState);
-//     }
-//   }
-// }
 
 void onGet_reboot() {
   Serial.println("onGet_reboot(...)");
